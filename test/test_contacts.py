5be69b723b00537d1ef2272c66f28dfb8da094de
vikto <viktoria.nazarova4949@gmail.com>
2026-09-15 21:05:35 +0300

    Login Tests Negative

--- test/test_contacts.py
+++ test/test_contacts.py
@@ -1,4 +1,12 @@
+from dataclasses import asdict
 
 
 class TestContacts:
     def test_add_contact(self, session, add_contact_url, auth_header, random_contact):
+        response=session.post(
+            add_contact_url,
+            headers=auth_header,
+            json=asdict(random_contact)
+        )
+        print(response.json())
+        assert response.status_code == 200
